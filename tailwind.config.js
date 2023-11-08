d23608cc16aa0ac84ed7462fca3f69da6f34833b
shadow <lightshadow416@gmail.com>
2023-11-08 20:05:12 +0100

    project core was build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        orange: "#FF5500",
+        paleOrange: "#FED6C6",
+        veryDarkBlue: "#1B1E34",
+        darkGrayishBlue: "#3B426A",
+        grayishBlue: "#A0AFC0",
+        lightGrayishBlue: "#F0F5FC",
+        blackLightbox: "rgba(0, 0, 0, 0.75)", // 75% opacity for lightbox background
       },
     },
   },
   plugins: [],
-}
+};
