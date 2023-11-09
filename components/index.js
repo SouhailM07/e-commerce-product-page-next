cf530fdc42169cef9917960a891082cf1d909eda
shadow <lightshadow416@gmail.com>
2023-11-09 14:14:49 +0100

    [Previewer] component was created

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as MyContainer } from "./MyContainer/MyContainer";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Preview } from "./Preview/Preview";
+export { default as Previewer } from "./Previewer/Previewer";
