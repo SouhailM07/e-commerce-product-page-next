413ddd44b813aea859b95bfb14a5c6c874f3ba7d
shadow <lightshadow416@gmail.com>
2023-11-09 13:43:43 +0100

    [Preview] component was created

--- components/MyContainer/MyContainer.jsx
+++ components/MyContainer/MyContainer.jsx
@@ -1,12 +1,15 @@
 // style
 import "./mycontainer.css";
 // components
-import { Navbar } from "@/components";
+import { Navbar, Preview } from "@/components";
 
 export default function MyContainer() {
   return (
     <>
       <Navbar />
+      <main className="max-w-[85rem] mt-[3rem] mx-auto">
+        <Preview />
+      </main>
     </>
   );
 }
---
+++ components/Preview/Preview.jsx
@@ -0,0 +1,8 @@
+import "./preview.css";
+export default function Preview() {
+  return (
+    <>
+      <article>Preview</article>
+    </>
+  );
+}
