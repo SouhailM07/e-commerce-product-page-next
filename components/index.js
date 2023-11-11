0d2e2d61c389df330097ad16f803ce26150243a2
shadow <lightshadow416@gmail.com>
2023-11-11 16:59:39 +0100

    [ArrowBtn] component was created

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as Preview } from "./Preview/Preview";
 export { default as Previewer } from "./Previewer/Previewer";
 export { default as Info } from "./Info/Info";
+export { default as ArrowBtn } from "./ArrowBtn/ArrowBtn";
